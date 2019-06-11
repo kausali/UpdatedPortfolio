085169c1ae1f592cff4d7696a944b9f107918004
kausali <kausali.lama@gmail.com>
2019-06-10 22:16:09 -0500

    update portfolio

---
+++ js/intro.js
@@ -0,0 +1,18 @@
+// header-content-inner
+
+var showText = function (target, message, index, interval) {   
+    if (index <= message.length) {
+        
+      $(target).append(message[index++]);
+      setTimeout( function () 
+      {
+          showText(target, message, index, interval); 
+        }, interval);
+    }
+  }
+$(function () {
+  showText("#kausali1", "FULLSTACK DEVELOPER", 0, 120); 
+  });
+
+
+  
