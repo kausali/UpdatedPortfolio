6d6e45c647532604b9188232ed89e99a28a18e75
kausali <kausali.lama@gmail.com>
2019-06-16 16:09:40 -0500

    making it better

--- js/particles.js
+++ js/particles.js
@@ -81,8 +81,8 @@ var pJS = function(tag_id, params){
           bounce: false,
           attract: {
             enable: false,
-            rotateX: 3000,
-            rotateY: 3000
+            rotateX: 5000,
+            rotateY: 5000
           }
         },
         array: []
