4cebc51fc357c7eb5d54a9de534be21e66311f39
kausali <kausali.lama@gmail.com>
2019-06-15 22:29:37 -0500

    make it better

--- js/intro.js
+++ js/intro.js
@@ -11,7 +11,10 @@ var showText = function (target, message, index, interval) {
     }
   }
 $(function () {
-  showText("#kausali1", "FULLSTACK DEVELOPER", 0, 120); 
+    // //var name = ('h1').append('Now is the time for all good men...');​
+    // showText("#homeHeading", "KAUSALI LAMA", 0, 150);
+    // setInterval(0,1000); 
+  showText(".kausali1", "FULLSTACK DEVELOPER", 0, 150); 
   });
 
 
